fc602353f4ff46d630e46e61295cf3566146e2d4
Jasper Dijt <jasperdijt@gmail.com>
2015-09-01 14:45:41 +0200

    Euler 49 cleanup.

--- Lab1/playground/jasper/Euler/Euler49.hs
+++ Lab1/playground/jasper/Euler/Euler49.hs
@@ -3,26 +3,27 @@ module Euler49 where
 import Primes
 import Data.List
 
--- Primes larger than 3340 result cause the last prime of the triple to be larger than 10000
+-- Uses primes from Primes.hs
 fourDigitPrimes :: [Integer]
-fourDigitPrimes = [p | p <- (takeWhile ((3340)>) primes), p >= 1000] 
+fourDigitPrimes = [p | p <- (takeWhile (10000>) primes), p >= 1000] 
 
 
---Generate prime triples with the 3330 difference
+-- Generate prime triples with the 3330 difference
+-- Primes larger than 3340 result cause the last prime of the triple to be larger than 10000
+-- uses prime from Primes.hs
 primeTriples :: [(Integer, Integer, Integer)]
-primeTriples = [(a, b, c) | a <- fourDigitPrimes, let b = a+3330, let c = b+3330, prime b, prime c]
+primeTriples = [(a, b, c) | a <- takeWhile (3340>=) fourDigitPrimes, 
+							let b = a+3330, 
+							let c = b+3330, 
+							(prime b) && (prime c)]
 
-
---Check if two numbers are permutations of each other:
-isPermutation :: Integer -> Integer -> Bool
-isPermutation a b = (sort$toString a) == (sort$toString b)
-	where
-	toString :: Integer -> String
-	toString i = show i
 
 isPermutedTriple :: (Integer, Integer, Integer) -> Bool
 isPermutedTriple (a,b,c) = (isPermutation a b) && (isPermutation a c)
-
+	where
+	isPermutation :: Integer -> Integer -> Bool
+	isPermutation a b = (sort$show a) == (sort$show b)
+	
 
 permutedPrimeTriples :: [(Integer, Integer, Integer)]
 permutedPrimeTriples = filter isPermutedTriple primeTriples
