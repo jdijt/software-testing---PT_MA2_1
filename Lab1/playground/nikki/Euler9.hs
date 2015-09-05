7346cb6645935a2999d214643982db62446267c4
Nikki van Ommeren <Nikki@Nikkis-MacBook-Pro.local>
2015-09-05 09:40:26 +0200

    Adapted Euler 9

--- Lab1/playground/nikki/Euler9.hs
+++ Lab1/playground/nikki/Euler9.hs
@@ -9,10 +9,11 @@ Nikki van Ommeren
 
 module Euler9 where
 
+-- |Returns all Pythagoras triples which has the value z from a list 
+triplet :: [Integer] -> [(Integer, Integer, Integer)]
+triplet [] = []
+triplet (z:zs) = [ (x,y,z) | x <- [2..z], y <- [2..z], z^2 == x^2 + y^2] ++ triplet zs
 
-triplet' :: [Integer] -> [(Integer, Integer, Integer)]
-triplet' [] = []
-triplet' (z:zs) = [ (x,y,z) | x <- [2..z], y <- [2..z], z^2 == x^2 + y^2] ++ triplet' zs
-
-triplet :: [(Integer, Integer, Integer)]
-triplet = triplet' [2..]
+-- |Finds the product of the Pythagoras Triple for which x + y + z = 1000
+findTriplet :: Integer
+findTriplet = head (take 1 [ (x*y*z) | (x,y,z) <- triplet [400..], x + y + z == 1000])
