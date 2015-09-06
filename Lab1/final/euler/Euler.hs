1548efec33ece2bf24a595357ac122e9a153bb5a
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-09-06 14:43:32 +0200

    Lab1: Added final version for euler 49

--- Lab1/final/euler/Euler.hs
+++ Lab1/final/euler/Euler.hs
@@ -1,6 +1,7 @@
 module Euler where
 
 import Primes
+import Data.List
 
 -- For a < b < c
 -- We can conclude that a <= 333 and b <= 499
@@ -11,3 +12,18 @@ solution9 = head [a * b * c | a <- [1..333], b <- [a..499], let c = 1000 - a - b
 solution10 :: Integer
 solution10 = sum (takeWhile (<2000000) primes)
 
+
+-- Check for all primes up to 3339 if the other triplet members (+3330 and +6660)
+-- are permutation of the prime and itself is a prime
+-- excluding the solution 1487
+solution49 :: Integer
+solution49 = concatTriples solution' (solution' + 3330) (solution' + 6660) where
+    solution' = head $ filter (\x -> prime x && isPrimeAndPermutation x (x + 3330) && isPrimeAndPermutation x (x + 6660)) $ ([1000..3339] \\ [1487])
+
+concatTriples a b c = 100000000 * a + 10000 * b + c
+
+isPrimeAndPermutation :: Integer -> Integer -> Bool
+isPrimeAndPermutation x y = prime y && isPermutation x y where
+
+isPermutation :: Integer -> Integer -> Bool
+isPermutation x y = (sort . show) x == (sort . show) y
