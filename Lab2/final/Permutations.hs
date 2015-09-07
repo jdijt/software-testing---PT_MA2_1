20d7a8308915e6cfd15bbeeeffa4059fc2386eab
Jasper Dijt <jasperdijt@gmail.com>
2015-09-07 15:04:01 +0200

    Testable properties.

--- Lab2/final/Permutations.hs
+++ Lab2/final/Permutations.hs
@@ -6,3 +6,15 @@ isPermutation :: Eq a => [a] -> [a] -> Bool
 isPermutation [] [] = True
 isPermutation [] _ = False
 isPermutation (x:xs) ys = x `elem` ys && (isPermutation xs (delete x ys))
+
+
+-- Testable properties
+isEqualLength :: [a] -> [a] -> Bool
+isEqualLength a b = length a == length b
+
+-- Based on: For each element a of the first list there must be an element b in the second list sucht that a == b
+containsAll :: Eq a => [a] -> [a] -> Bool
+containsAll a b = all (\x -> any (==x) b) a
+
+
+
