0f44aec95add0a01d6ffa908e691df38b3c6d56d
Jasper Dijt <jasperdijt@gmail.com>
2015-09-08 10:58:32 +0200

    Change to guarded expressions.

--- Lab2/final/IBAN.hs
+++ Lab2/final/IBAN.hs
@@ -14,14 +14,16 @@ toDigits xs = read (concat $ map show xs) :: Int
 
 adaptLetters :: String -> [Int]
 adaptLetters [] = []
-adaptLetters (x:xs) = if isAlpha x then (ord x - 55):adaptLetters xs
-	                  else digitToInt x:adaptLetters xs
+adaptLetters (x:xs)
+	| isAlpha x	= (ord x - 55):adaptLetters xs
+	| otherwise = digitToInt x:adaptLetters xs
 
 moveHeader :: String -> String
 moveHeader xs = drop 4 xs ++ take 4 xs
 
 removeSpaces :: String -> String
 removeSpaces [] = []
-removeSpaces (x:xs) = if isSpace x then removeSpaces xs
-	                  else x:removeSpaces xs
+removeSpaces (x:xs)
+	| isSpace x	= removeSpaces xs
+	| otherwise	= x:removeSpaces xs
 
