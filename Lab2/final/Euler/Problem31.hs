2d23006146aa6315ca421159b01a8015cc4f7ae7
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-09-13 16:54:41 +0200

    Lab2: Improvements euler 31

--- Lab2/final/Euler/Problem31.hs
+++ Lab2/final/Euler/Problem31.hs
@@ -1,13 +1,10 @@
 module Problem31 where
 
-import Data.List 
-
 count :: Integer -> [Integer] -> Integer
-count x [] = 0
-count x y | x < 0 = 0
+count _ [] = 0
+count x (y:ys) | x < 0 = 0
           | x == 0 = 1
-          | otherwise = count (x - m) y + count x (delete m y) 
-            where m = maximum y
+          | otherwise = count (x - y) (y:ys) + count x ys
 
 solution31 :: Integer
-solution31 = count 200 coins where coins = [200,100,50,20,10,5,2,1]
+solution31 = count 200 [200,100,50,20,10,5,2,1]
