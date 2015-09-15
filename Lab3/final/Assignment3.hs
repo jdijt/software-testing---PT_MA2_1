11550a8d1de62a96a2a1ce68eb74640c47a1c177
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-09-15 09:41:08 +0200

    Lab3: Added nnf and arrowfree to assignment 3

--- Lab3/final/Assignment3.hs
+++ Lab3/final/Assignment3.hs
@@ -25,3 +25,24 @@ testLiftCnj = and [liftCnj (Prop 1) == Prop 1,
             liftCnj (Cnj [Prop 1, Prop 2]) == Cnj [Prop 1, Prop 2],
             liftCnj (Cnj [Dsj [Prop 1, Prop 2], Prop 3]) == Cnj [Dsj [Prop 1, Prop 2], Prop 3],
             liftCnj (Dsj [Cnj [Prop 1, Prop 2], Prop 3]) == Cnj [Dsj [Prop 1, Prop 3], Dsj [Prop 2, Prop 3]]]
+
+nnf :: Form -> Form 
+nnf (Prop x) = Prop x
+nnf (Neg (Prop x)) = Neg (Prop x)
+nnf (Neg (Neg f)) = nnf f
+nnf (Cnj fs) = Cnj (map nnf fs)
+nnf (Dsj fs) = Dsj (map nnf fs)
+nnf (Neg (Cnj fs)) = Dsj (map (nnf.Neg) fs)
+nnf (Neg (Dsj fs)) = Cnj (map (nnf.Neg) fs)
+
+arrowfree :: Form -> Form 
+arrowfree (Prop x) = Prop x 
+arrowfree (Neg f) = Neg (arrowfree f)
+arrowfree (Cnj fs) = Cnj (map arrowfree fs)
+arrowfree (Dsj fs) = Dsj (map arrowfree fs)
+arrowfree (Impl f1 f2) = 
+  Dsj [Neg (arrowfree f1), arrowfree f2]
+arrowfree (Equiv f1 f2) = 
+  Dsj [Cnj [f1', f2'], Cnj [Neg f1', Neg f2']]
+  where f1' = arrowfree f1
+        f2' = arrowfree f2
