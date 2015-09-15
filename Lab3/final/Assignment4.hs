e9b6d79609dc741829328d90641486bda2fb2b2d
Nikki van Ommeren <Nikki@wcw-stud-145-109-18-3.wireless.uva.nl>
2015-09-15 13:19:42 +0200

    improved assignment4

--- Lab3/final/Assignment4.hs
+++ Lab3/final/Assignment4.hs
@@ -1,6 +1,15 @@
 module Assignment4 where
 
 import PropositionGenerator
+import Assignment3
 import Lecture3
 import Test.QuickCheck
 import Data.List
+
+compareTest :: IO Bool
+compareTest = do 
+        randomForm <- (generateValidForm $ generateProp)
+        let valuations = allVals randomForm
+        let result = map (\x -> evl x randomForm) valuations
+        let cnfResult = map (\x -> evl x (toCNF randomForm)) valuations
+        return (result == cnfResult)
