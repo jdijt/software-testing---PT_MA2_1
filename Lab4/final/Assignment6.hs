49c111d0ac0766e8f311b8b2074cd6101db12bfa
pacbeckh <pacbeckh@users.noreply.github.com>
2015-09-22 11:53:47 +0200

    Sort result of trClos

--- Lab4/final/Assignment6.hs
+++ Lab4/final/Assignment6.hs
@@ -12,7 +12,7 @@ import Data.Function
 --  - No duplicates
 --  - result is a transitively closed.
 trClos :: Ord a => Rel a -> Rel a
-trClos r = fix (\f x -> if x == trIteration r x then x else f ( trIteration r x) ) r 
+trClos r = sort $ fix (\f x -> if x == trIteration r x then x else f ( trIteration r x) ) r
     where
     trIteration :: Ord a => Rel a -> Rel a -> Rel a
     trIteration b x = nub $ x ++ (x @@ b)
