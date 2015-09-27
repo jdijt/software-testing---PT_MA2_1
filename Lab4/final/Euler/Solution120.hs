b1919804b8abd3f3e525a1320c9fb682e18b3c86
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-09-27 12:23:22 +0200

    Lab 4: Nicer solution to euler

--- Lab4/final/Euler/Solution120.hs
+++ Lab4/final/Euler/Solution120.hs
@@ -1,17 +1,4 @@
 module Solution120 where
 
-totalRemains :: [Integer] -> Integer
-totalRemains [] = 0
-totalRemains (x:xs) | odd x = remainOdd + totalRemains xs
-					| otherwise = remainEven + totalRemains xs
-						    where remainOdd = x * (x - 1)
-						    	  remainEven = x * (x - 2)
-
--- Solution Mats
--- parity :: Integer -> Integer
--- parity = (`mod` 2)
--- totalRemains :: [Integer] -> Integer
--- totalRemains xs = sum (map (\x -> x * (x - 2 + parity x)) xs)
-
 solution120 :: Integer
-solution120 = totalRemains [3..1000]
+solution120 = sum (map (\x -> x * (x - 2 + x `mod` 2)) [3..1000])
