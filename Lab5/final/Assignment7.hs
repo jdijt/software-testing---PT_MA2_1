777494cc871becca84b69453b5ce889a29af4fff
Jasper Dijt <jasperdijt@gmail.com>
2015-09-29 17:34:08 +0200

    Lab5 :: Assignment 7, smaller and more readable.

--- Lab5/final/Assignment7.hs
+++ Lab5/final/Assignment7.hs
@@ -7,17 +7,13 @@ import Assignment1 as A1
 getAvgHintsA1Sudokus :: Int -> IO ()
 getAvgHintsA1Sudokus n = do
     is <- genRandomInstances (A1.genRandomSudoku >>= A1.genProblem) n
-    print ((fromIntegral $ sumHints A1.filledPositions is) / (fromIntegral n))
+    print $ (fromIntegral $ sum $ map (length.A1.filledPositions.fst) is) / (fromIntegral n)
 
 getAvgHintsL5Sudokus :: Int -> IO ()
 getAvgHintsL5Sudokus n = do
     is <- genRandomInstances (L5.genRandomSudoku >>= L5.genProblem) n
-    print ((fromIntegral $ sumHints L5.filledPositions is) / (fromIntegral n))
+    print $ (fromIntegral $ sum $ map (length.L5.filledPositions.fst) is) / (fromIntegral n)
 
--- Could be: (Sudoku -> [(row,column)]) -> [Node] -> Int
--- But we are using A1. and L5. types mixed trough each other, so set up generically
-sumHints :: (a -> [b]) -> [(a,c)] -> Int
-sumHints f = foldl (\x y -> x + ((length.f.fst) y)) 0 
 
 genRandomInstances :: (IO a) -> Int -> IO [a]
 genRandomInstances _ 0 = return []
