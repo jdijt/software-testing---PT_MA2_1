b00b8f934e3ffb6773c5a7d0b65ca8751bcaeb3c
pacbeckh <pacbeckh@users.noreply.github.com>
2015-09-29 11:22:32 +0200

    Only generate valid solutions

--- Lab5/final/Assignment4.hs
+++ Lab5/final/Assignment4.hs
@@ -3,14 +3,28 @@ where
 
 import Lecture5
 
-possibleBoxes :: [([Row],[Column])]
-possibleBoxes = [ (x,y) | x <- blocks, y <- blocks]
+possibleBlocks :: [([Row],[Column])]
+possibleBlocks = [ (x,y) | x <- blocks, y <- blocks]
 
-generateEmptyBoxes :: Int -> IO ()
-generateEmptyBoxes n = do
-  [r] <- rsolveNs [emptyN]
-  s <- randomize possibleBoxes
-  let t = take n s
-  let u = concatMap (\(x,y) -> [(a,b)| a <- x, b <-y ]) t
-  let v = foldl eraseN r u
-  showNode v
+minimalizeBlocks :: Node -> [[(Row,Column)]] -> Node
+minimalizeBlocks n [] = n
+minimalizeBlocks n (x:xs) | uniqueSol n' = minimalizeBlocks n' xs
+                          | otherwise    = minimalizeBlocks n  xs
+    where n' = foldl eraseN n x
+
+genProblemEmptyBlocks :: Node -> IO Node
+genProblemEmptyBlocks n = do
+                            -- First take out as many blocks as possible
+                            rblocks <- randomize possibleBlocks
+                            let blocks' = map (\(x,y) -> [(a,b)| a <- x, b <-y ]) rblocks
+                            let n' = minimalizeBlocks n blocks'
+                            -- Then try to minimalize the remainder
+                            i <- randomize (filledPositions (fst n'))
+                            return (minimalize n' i)
+
+mainEmptyBlocks :: IO ()
+mainEmptyBlocks = do
+                    [r] <- rsolveNs [emptyN]
+                    showNode r
+                    s  <- genProblemEmptyBlocks r
+                    showNode s
