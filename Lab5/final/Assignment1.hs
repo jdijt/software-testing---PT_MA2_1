0fef052d6653458db2017ec743ed2a9993bedbbd
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-09-29 10:58:48 +0200

    Lab 5: Fix for assignment 1

--- Lab5/final/Assignment1.hs
+++ Lab5/final/Assignment1.hs
@@ -1,6 +1,6 @@
-module Lecture5
+module Assignment1
 
-where 
+where
 
 import Data.List
 import System.Random
@@ -74,8 +74,8 @@ exampleNRC = [[0,0,0,3,0,0,0,0,0],
 -- | Source: Lecture 5
 ----------------------------------------------------------------------
 
-type Row    = Int 
-type Column = Int 
+type Row    = Int
+type Column = Int
 type Value  = Int
 type Grid   = [[Value]]
 
@@ -91,7 +91,7 @@ showVal 0 = " "
 showVal d = show d
 
 showRow :: [Value] -> IO()
-showRow [a1,a2,a3,a4,a5,a6,a7,a8,a9] = 
+showRow [a1,a2,a3,a4,a5,a6,a7,a8,a9] =
  do  putChar '|'         ; putChar ' '
      putStr (showVal a1) ; putChar ' '
      putStr (showVal a2) ; putChar ' '
@@ -122,9 +122,9 @@ sud2grid :: Sudoku -> Grid
 sud2grid s = [ [ s (r,c) | c <- [1..9] ] | r <- [1..9] ]
 
 grid2sud :: Grid -> Sudoku
-grid2sud gr = \ (r,c) -> pos gr (r,c) 
-  where 
-  pos :: [[a]] -> (Row,Column) -> a 
+grid2sud gr = \ (r,c) -> pos gr (r,c)
+  where
+  pos :: [[a]] -> (Row,Column) -> a
   pos gr (r,c) = (gr !! (r-1)) !! (c-1)
 
 showSudoku :: Sudoku -> IO()
@@ -152,22 +152,22 @@ injective :: Eq a => [a] -> Bool
 injective xs = nub xs == xs
 
 rowInjective :: Sudoku -> Row -> Bool
-rowInjective s r = injective vs where 
+rowInjective s r = injective vs where
    vs = filter (/= 0) [ s (r,i) | i <- positions ]
 
 colInjective :: Sudoku -> Column -> Bool
-colInjective s c = injective vs where 
+colInjective s c = injective vs where
    vs = filter (/= 0) [ s (i,c) | i <- positions ]
 
 subgridInjective :: Sudoku -> (Row,Column) -> Bool
-subgridInjective s (r,c) = injective vs where 
+subgridInjective s (r,c) = injective vs where
    vs = filter (/= 0) (subGrid s (r,c))
 
 extend :: Sudoku -> ((Row,Column),Value) -> Sudoku
 extend = update
 
-update :: Eq a => (a -> b) -> (a,b) -> a -> b 
-update f (y,z) x = if x == y then z else f x 
+update :: Eq a => (a -> b) -> (a,b) -> a -> b
+update f (y,z) x = if x == y then z else f x
 
 type Constraint = (Row,Column,[Value])
 
@@ -180,9 +180,9 @@ solved  :: Node -> Bool
 solved = null . snd
 
 extendNode :: Node -> Constraint -> [Node]
-extendNode (s,constraints) (r,c,vs) = 
+extendNode (s,constraints) (r,c,vs) =
    [(extend s ((r,c),v),
-     sortBy length3rd $ 
+     sortBy length3rd $
          prune (r,c,v) constraints) | v <- vs ]
 
 length3rd :: (a,b,[c]) -> (a,b,[c]) -> Ordering
@@ -192,18 +192,18 @@ sameblock :: (Row,Column) -> (Row,Column) -> Bool
 sameblock (r,c) (x,y) = bl r == bl x && bl c == bl y
 
 initNode :: Grid -> [Node]
-initNode gr = let s = grid2sud gr in 
-              if (not . consistent) s then [] 
+initNode gr = let s = grid2sud gr in
+              if (not . consistent) s then []
               else [(s, constraints s)]
 
 openPositions :: Sudoku -> [(Row,Column)]
-openPositions s = [ (r,c) | r <- positions,  
-                            c <- positions, 
+openPositions s = [ (r,c) | r <- positions,
+                            c <- positions,
                             s (r,c) == 0 ]
 
-constraints :: Sudoku -> [Constraint] 
-constraints s = sortBy length3rd 
-    [(r,c, freeAtPos s (r,c)) | 
+constraints :: Sudoku -> [Constraint]
+constraints s = sortBy length3rd
+    [(r,c, freeAtPos s (r,c)) |
                        (r,c) <- openPositions s ]
 
 data Tree a = T a [Tree a] deriving (Eq,Ord,Show)
@@ -211,25 +211,25 @@ data Tree a = T a [Tree a] deriving (Eq,Ord,Show)
 exmple1 = T 1 [T 2 [], T 3 []]
 exmple2 = T 0 [exmple1,exmple1,exmple1]
 
-grow :: (node -> [node]) -> node -> Tree node 
+grow :: (node -> [node]) -> node -> Tree node
 grow step seed = T seed (map (grow step) (step seed))
 
-count :: Tree a -> Int 
+count :: Tree a -> Int
 count (T _ ts) = 1 + sum (map count ts)
 
-search :: (node -> [node]) 
+search :: (node -> [node])
        -> (node -> Bool) -> [node] -> [node]
 search children goal [] = []
-search children goal (x:xs) 
+search children goal (x:xs)
   | goal x    = x : search children goal xs
   | otherwise = search children goal ((children x) ++ xs)
 
 solveNs :: [Node] -> [Node]
-solveNs = search succNode solved 
+solveNs = search succNode solved
 
 succNode :: Node -> [Node]
 succNode (s,[]) = []
-succNode (s,p:ps) = extendNode (s,ps) p 
+succNode (s,p:ps) = extendNode (s,ps) p
 
 solveAndShow :: Grid -> IO[()]
 solveAndShow gr = solveShowNs (initNode gr)
@@ -262,7 +262,7 @@ example2 = [[0,3,0,0,7,0,0,0,0],
 example3 :: Grid
 example3 = [[1,0,0,0,3,0,5,0,4],
             [0,0,0,0,0,0,0,0,3],
-            [0,0,2,0,0,5,0,9,8], 
+            [0,0,2,0,0,5,0,9,8],
             [0,0,9,0,0,0,0,3,0],
             [2,0,0,0,0,0,0,0,7],
             [8,0,3,0,9,1,0,6,0],
@@ -305,8 +305,8 @@ getRandomItem xs = do n <- getRandomInt maxi
                    where maxi = length xs - 1
 
 randomize :: Eq a => [a] -> IO [a]
-randomize xs = do y <- getRandomItem xs 
-                  if null y 
+randomize xs = do y <- getRandomItem xs
+                  if null y
                     then return []
                     else do ys <- randomize (xs\\y)
                             return (head y:ys)
@@ -315,34 +315,34 @@ sameLen :: Constraint -> Constraint -> Bool
 sameLen (_,_,xs) (_,_,ys) = length xs == length ys
 
 getRandomCnstr :: [Constraint] -> IO [Constraint]
-getRandomCnstr cs = getRandomItem (f cs) 
+getRandomCnstr cs = getRandomItem (f cs)
   where f [] = []
         f (x:xs) = takeWhile (sameLen x) (x:xs)
 
 rsuccNode :: Node -> IO [Node]
 rsuccNode (s,cs) = do xs <- getRandomCnstr cs
-                      if null xs 
+                      if null xs
                         then return []
-                        else return 
+                        else return
                           (extendNode (s,cs\\xs) (head xs))
 
 rsolveNs :: [Node] -> IO [Node]
 rsolveNs ns = rsearch rsuccNode solved (return ns)
 
 rsearch :: (node -> IO [node]) -> (node -> Bool) -> IO [node] -> IO [node]
-rsearch succ goal ionodes = 
-  do xs <- ionodes 
-     if null xs 
+rsearch succ goal ionodes =
+  do xs <- ionodes
+     if null xs
        then return []
-       else 
-         if goal (head xs) 
+       else
+         if goal (head xs)
            then return [head xs]
            else do ys <- rsearch succ goal (succ (head xs))
-                   if (not . null) ys 
+                   if (not . null) ys
                       then return [head ys]
                       else if null (tail xs) then return []
-                           else 
-                             rsearch 
+                           else
+                             rsearch
                                succ goal (return $ tail xs)
 
 genRandomSudoku :: IO Node
@@ -352,7 +352,7 @@ genRandomSudoku = do [r] <- rsolveNs [emptyN]
 randomS = genRandomSudoku >>= showNode
 
 uniqueSol :: Node -> Bool
-uniqueSol node = singleton (solveNs [node]) where 
+uniqueSol node = singleton (solveNs [node]) where
   singleton [] = False
   singleton [x] = True
   singleton (x:y:zs) = False
@@ -385,4 +385,3 @@ main = do [r] <- rsolveNs [emptyN]
           showNode r
           s  <- genProblem r
           showNode s
-
