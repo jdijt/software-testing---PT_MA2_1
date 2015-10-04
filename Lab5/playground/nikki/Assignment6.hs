46fbb0399eff8510e7ec792629a4f2eac45c6f82
pacbeckh <pacbeckh@users.noreply.github.com>
2015-10-04 12:54:48 +0200

    Improved code

--- Lab5/playground/nikki/Assignment6.hs
+++ Lab5/playground/nikki/Assignment6.hs
@@ -2,25 +2,23 @@ module Assignment6 where
 
 import Lecture5 hiding (main, minimalize, genProblem)
 import Control.Monad
---import Control.Monad.LoopWhile
 
 getThird :: (a,a,[a]) -> [a]
 getThird (_,_,[]) = []
 getThird (_,_,(x:xs)) = x : xs 
 
-minimalize :: Int -> [(Int,Row,Column,[Value])] -> Node -> [(Row,Column)] -> ([(Int,Row,Column,[Value])],Node)
+minimalize :: Int -> [Int] -> Node -> [(Row,Column)] -> ([Int],Node)
 minimalize level as n [] = (as,n)
-minimalize level as n ((r,c):rcs) | uniqueSol n' && numberPossibleVal <= level = minimalize level (a:as) n' rcs
+minimalize level as n ((r,c):rcs) | uniqueSol n' && numberPossibleVal <= level = minimalize level (numberPossibleVal:as) n' rcs
                                   | otherwise    = minimalize level as n rcs
                 where n' = eraseN n (r,c)
                       numberPossibleVal = length (getThird (findConstraint r c (snd n')))
-                      a = (numberPossibleVal,r,c, getThird (findConstraint r c (snd n')))
 
 findConstraint :: Row -> Column -> [Constraint] -> Constraint
 findConstraint r c (z@(x,y,_):xs) = if (r == x) && (c == y) then z
                             else findConstraint r c xs
 
-genProblem :: Int -> Node -> IO ([(Int,Row,Column,[Value])],Node)
+genProblem :: Int -> Node -> IO ([Int],Node)
 genProblem level n = do 
                       ys <- randomize xs
                       return (minimalize level [] n ys)
@@ -29,10 +27,9 @@ genProblem level n = do
 --main :: Int -> IO Int
 main level = do 
               [r] <- rsolveNs [emptyN]
---              showNode r
+              showNode r
               (i,s)  <- genProblem level r
---              showNode s;
---              print (maximum [x | (x,y,z,t) <- i]);
---              print (sum $ map (\(x, _, _, _) -> x) i)
-              return (sum $ map (\(x, _, _, _) -> x) i)
-              -- TODO print "Level is: " ++
+              showNode s;
+              let difficulty = sum i
+              print ("The minimal probly for n = " ++ (show level) ++ " has a difficulty of " ++ (show difficulty) ++ " on a scale where 30 is really easy and 130 is insane.")
+              return (difficulty)
