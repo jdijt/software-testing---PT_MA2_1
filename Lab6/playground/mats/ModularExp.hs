ce5617e45e10346764c09c61be946569c3b1d9a1
Mats Stijlaart <m.stijlaart@avisi.nl>
2015-10-06 10:09:15 +0200

    Lab 6: Added binary optimal solution

--- Lab6/playground/mats/ModularExp.hs
+++ Lab6/playground/mats/ModularExp.hs
@@ -2,7 +2,15 @@ module ModularExp
 
 where
 
+import Numeric
+import Data.Char
+
+toBase :: Integer -> Integer -> [Int]
+toBase base num = map digitToInt(showIntAtBase base intToDigit num "")
+
+base2modPowers :: Integer -> [Int] -> Integer -> [Integer]
+base2modPowers a xs c = foldr (\_ (b:bs) -> (b * b) `mod` c : b :  bs)  [a `mod` c] (init xs)
 
 exM :: Integer -> Integer -> Integer -> Integer
-exM x 1 n = rem x n
-exM x y n = rem (x * exM x (y-1) n ) n
+exM a b c = product (map fst $ filter (\q -> snd q == 1) (zip (base2modPowers a bBits c) bBits)) `mod` c where
+  bBits = toBase 2 b
