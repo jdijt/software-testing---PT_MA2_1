db4f5198973bd2d8c8d01ddb38c84d37dbe485d8
pacbeckh <pacbeckh@users.noreply.github.com>
2015-10-07 16:52:35 +0200

    Yay a working solution!

--- Lab6/playground/paco/Assignment1.hs
+++ Lab6/playground/paco/Assignment1.hs
@@ -2,32 +2,23 @@ module Assignment1
 
 where
 
-import Numeric (showHex, showIntAtBase)
+import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
-import Lecture6 hiding (exM)
+import Lecture6 (multM)
 
 --Taken from http://stackoverflow.com/a/1959792/2307346
-toBinary :: Int -> String
-toBinary c = reverse $ showIntAtBase 2 intToDigit c ""
+toBinary :: Int -> [Char]
+toBinary n = showIntAtBase 2 intToDigit n ""
 
---getPowersOfTwo :: [Char] -> [Int]
---getPowersOfTwo [] = []
---getPowersOfTwo ('1':t) = 2 ^ (length t) : getPowersOfTwo t
---getPowersOfTwo ('0':t) = getPowersOfTwo t
+getPowersOfTwo :: Integer -> Integer -> Int -> [Integer]
+getPowersOfTwo a c max = foldl (\xs@(h:_) _ -> multM h h c : xs) [a] [1..max]
 
---getModPower :: Integer -> Integer -> [Int] -> [Integer]
---getModPower a b xs = map (\x -> (a ^ x) `mod` b) xs
+shouldCombine :: Char -> Integer -> Integer
+shouldCombine '1' n = n
+shouldCombine _ _ = 1
 
-getPowersOfTwo a b binary = foldl (\x y -> multM (head x) (head x) b : x) [a] [1..(length(binary) -1)]
-
---myzip powers binary =
-
-shouldTake :: Char -> Integer -> Integer
-shouldTake '1' n = n
-shouldTake _ _ = 1
-
---modMultiply :: Integer -> [Integer] -> Integer
---modMultiply b modPowers = foldr (\x y -> multM x y b) 1 modPowers `mod` b
---
---exM :: Integer -> Int -> Integer -> Integer
---exM a c b = modMultiply b $ getModPower a b $ getPowersOfTwo $ toBinary c
+exM :: Integer -> Int -> Integer -> Integer
+exM a b 0 = error "mod 0 not allowed"
+exM a b c = (product $ zipWith shouldCombine binary (getPowersOfTwo a c (length binary - 1))) `mod` c
+    where
+        binary = toBinary b
